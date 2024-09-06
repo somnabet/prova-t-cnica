9595537d814235fa34ebcc594639024aa640c069
Bet Oliver <eoliveco11@gmail.com>
2024-09-06 09:11:20 +0200

    imatge+nom random d'API + botó per canviar. ACABAT

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const DOG_CEO_API = "https://dog.ceo/api/breeds/image/random";
+const RANDOM_USER_API = "https://randomuser.me/api/";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dog, setDog] = useState("");
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    fetch(DOG_CEO_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setDog(data.message);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch(RANDOM_USER_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data.results[0].name.first);
+      });
+  }, []);
+
+  const onHandleSubmit = () => {
+    fetch(DOG_CEO_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setDog(data.message);
+      });
+    fetch(RANDOM_USER_API)
+      .then((res) => res.json())
+      .then((data) => {
+        setUser(data.results[0].name.first);
+      });
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main>
+      <img src={dog} alt="Imagen de un perro obtenida por la API dog.ceo"></img>
+      <h1>{user}</h1>
+      <button onClick={() => onHandleSubmit()}>♻️</button>
+    </main>
+  );
 }
 
-export default App
+export default App;
